5d68647e61f963974cd93f021d52828d2e9fe2a3
NonyeReeta <nonyeibeanu@gmail.com>
2024-04-06 03:09:48 +0100

    added local storage for tasks

--- src/app/auth/component/home/home.component.ts
+++ src/app/auth/component/home/home.component.ts
@@ -7,28 +7,37 @@ import {
   CdkDropList
 } from '@angular/cdk/drag-drop';
 import { CommonModule } from '@angular/common';
-
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { TaskService } from 'src/app/services/task.service';
 
 interface Task {
   title: string,
-  description: string
+  description: string,
+  status: string,
+  due_date: Date,
+  priority: string
 }
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CdkDropList, CdkDrag, CommonModule],
+  imports: [CdkDropList, CdkDrag, CommonModule, ReactiveFormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 
 export class HomeComponent {
 
-  pending_tasks:Task[] = [{title: 'TLorem ipsum dolor sit amet', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent fermentum euismod tempus.'}];
-  progress_tasks:Task[] = [{title: 'TLorem ipsum dolor sit amet', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent fermentum euismod tempus.'}, {title: 'TLorem ipsum dolor sit amet', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent fermentum euismod tempus.'}];
-  completed_tasks:Task[] = [{title: 'TLorem ipsum dolor sit amet', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent fermentum euismod tempus.'}];
+  pending_tasks:Task[] = [];
+  progress_tasks:Task[] = [];
+  completed_tasks:Task[] = [];
+
+  show_task:boolean = false;
+  task:any = {};
 
   show_new_task_modal:boolean = false;
+  is_operation_in_progress:boolean = false;
  
 
   drop(event: CdkDragDrop<Task[]>) {
@@ -45,11 +54,48 @@ export class HomeComponent {
     }
   };
 
+  constructor(
+    private taskService: TaskService
+  ) {}
+
+  taskForm: FormGroup = new FormGroup ({
+    title: new FormControl(null, Validators.required),
+    description: new FormControl(null, Validators.required),
+    due_date: new FormControl(null, Validators.required),
+    priority: new FormControl(null, Validators.required),
+    status: new FormControl(null, Validators.required)
+  })
+
   ngOnInit() {
     // load all task
     this.loadAllTasks();
-  }
+  };
 
-  loadAllTasks() {}
+  loadAllTasks() {
+    // load existing tasks
+    this.pending_tasks = this.taskService.getData('pending_tasks') || [];
+    this.progress_tasks = this.taskService.getData('progress_tasks') || [];
+    this.completed_tasks = this.taskService.getData('completed_tasks') || [];
+  };
+
+  AddTask() {
+    this.is_operation_in_progress = true;
+    // add new task to pending and set operation progress to false;
+    this.pending_tasks.push(this.taskForm.value);
+    this.taskService.saveData('pending_tasks', this.pending_tasks);
+    this.is_operation_in_progress = false;
+    this.show_new_task_modal = false;
+    console.log(this.taskForm.value);
+  };
+
+  viewTask(task: Task) {
+    this.task = task;
+  };
+
+  deleteTask(task: Task) {
+
+  };
+
+  updateTask(task: Task) {};
 
 }
---
+++ src/app/services/task.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+
+  constructor() { }
+
+    // save data to local storage
+    saveData(key: string, data: any) {
+      localStorage.setItem(key, JSON.stringify(data));
+    }
+  
+    // retrieve data from local storage
+    getData(key: string): any {
+      const data = localStorage.getItem(key);
+      return data ? JSON.parse(data) : null;
+    }
+}
