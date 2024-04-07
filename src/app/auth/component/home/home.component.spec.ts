70e8ecf9e6eef443870d079225cf102f6015c569
NonyeReeta <nonyeibeanu@gmail.com>
2024-04-07 15:24:20 +0100

    added filter by task status v2

--- src/app/auth/component/home/home.component.spec.ts
+++ src/app/auth/component/home/home.component.spec.ts
@@ -20,20 +20,4 @@ describe('HomeComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-
-  component = new HomeComponent();
-  
-  component.pending_tasks = [
-    { title: 'Task 1', description: 'Description 1', priority: 'high', status: 'pending', due_date: new Date(Date.now()), id: '' },
-    { title: 'Task 2', description: 'Description 2', priority: 'medium', status: 'pending', due_date: new Date(Date.now()), id: '' },
-  ];
-  component.progress_tasks = [
-    { title: 'Task 3', description: 'Description 3', priority: 'high', status: 'progress', due_date: new Date(Date.now()), id: '' },
-    { title: 'Task 4', description: 'Description 4', priority: 'low', status: 'progress', due_date: new Date(Date.now()), id: '' },
-  ];
-  component.completed_tasks = [
-    { title: 'Task 5', description: 'Description 5', priority: 'low', status: 'completed', due_date: new Date(Date.now()), id: '' },
-    { title: 'Task 6', description: 'Description 6', priority: 'medium', status: 'completed', due_date: new Date(Date.now()), id: '' },
-  ];
-});
 });
